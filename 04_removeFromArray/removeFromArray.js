9e2222eb1ea86cc32f1e244eec684e48cb39a018
Lane <lanedwofford@gmail.com>
2025-01-16 20:06:18 -0600

    Add functional-style solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,16 @@
+// const removeFromArray = function(arr, ...targetArgs) {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (targetArgs.includes(arr[i])) {
+//             arr.splice(i, 1);
+//             i--;
+//         }
+//     }
+//     return arr;
+// };
+
 const removeFromArray = function(arr, ...targetArgs) {
-    for (let i = 0; i < arr.length; i++) {
-        if (targetArgs.includes(arr[i])) {
-            arr.splice(i, 1);
-            i--;
-        }
-    }
-    return arr;
-};
+    return arr.filter(item => !targetArgs.includes(item));
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
