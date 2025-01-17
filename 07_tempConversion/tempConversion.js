3db09676756c5951818397f506510cac4ae784ec
Lane <lanedwofford@gmail.com>
2025-01-16 20:40:16 -0600

    Complete tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const realCel = (temp - 32) * 5 / 9;
+  return Math.round(realCel * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const realFahr = (temp * 9 / 5 + 32);
+  return Math.round(realFahr * 10) / 10;
 };
 
 // Do not edit below this line
