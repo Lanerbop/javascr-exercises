2dd9496d7e76cd2e334a33787478a3e60bf6224b
Lane <lanedwofford@gmail.com>
2025-01-16 20:04:33 -0600

    Complete removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...targetArgs) {
+    for (let i = 0; i < arr.length; i++) {
+        if (targetArgs.includes(arr[i])) {
+            arr.splice(i, 1);
+            i--;
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
